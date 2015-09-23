64c2062bf27880e19723de351ef27a4d0979f9b7
Steven Li Li <lli380@aucklanduni.ac.nz>
2015-09-23 23:17:54 +1200

    Add AdminConsole window to the project

---
+++ MPAid/Forms/AdminConsole.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MPAid
+{
+    public partial class AdminConsole : Form
+    {
+        public AdminConsole()
+        {
+            InitializeComponent();
+        }
+    }
+}
