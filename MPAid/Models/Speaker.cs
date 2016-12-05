bfecc8014d5348c188c59938fc72eb99a60fb182
JSCooke <jaydencooke@outlook.com>
2016-12-06 10:40:37 +1300

    Commented Speaker class.

--- MPAid/Models/Speaker.cs
+++ MPAid/Models/Speaker.cs
@@ -5,18 +5,27 @@ namespace MPAid.Models
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
-
+    /// <summary>
+    /// Entity class for mapping a speaker into the database.
+    /// </summary>
     [Table("Speaker")]
     public partial class Speaker
     {
+        /// <summary>
+        /// Unique identifier for each speaker.
+        /// </summary>
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int SpeakerId { get; set; }
-
+        /// <summary>
+        /// Unique name for each speaker.
+        /// </summary>
         [Required]
         [StringLength(64)]
         [Index(IsUnique = true)]
         public string Name { get; set; }
-
+        /// <summary>
+        /// Each speaker can be associated with a collection of recordings.
+        /// </summary>
         public virtual ICollection<Recording> Recordings { get; set; }
     }
 }
