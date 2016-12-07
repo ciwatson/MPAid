a3392fc8683ef8a12d52506a5919e9de43e24d64
JSCooke <jaydencooke@outlook.com>
2016-12-08 12:44:15 +1300

    Commented the (Apparently unused) VerticalProgressBar class.

--- MPAid/Components/VerticalProgressBar.cs
+++ MPAid/Components/VerticalProgressBar.cs
@@ -2,8 +2,15 @@
 
 namespace MPAid
 {
+    /// <summary>
+    /// Parameter overridden extension of the C# progress bar class.
+    /// *** Doesn't appear to be used by any other code, marked for deletion ***
+    /// </summary>
     public class VerticalProgressBar : ProgressBar
     {
+        /// <summary>
+        /// Adds a style parameter to the existing CreateParams method.
+        /// </summary>
         protected override CreateParams CreateParams
         {
             get
