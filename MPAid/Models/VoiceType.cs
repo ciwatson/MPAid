fe90f9d96b1ef537534fdc2adc8be63ecf78d7b8
ThomasRCoupe <thomasrcoupe97@gmail.com>
2016-12-20 14:46:24 +1300

    Commented the VoiceType file

--- MPAid/Models/VoiceType.cs
+++ MPAid/Models/VoiceType.cs
@@ -6,16 +6,26 @@ using System.Threading.Tasks;
 
 namespace MPAid.Models
 {
+    /// <summary>
+    /// Enum representing the VoiceType setting for a user.
+    /// </summary>
     public enum VoiceType
     {
         MASCULINE_HERITAGE, MASCULINE_MODERN, FEMININE_HERITAGE, FEMININE_MODERN
     }
 
+    /// <summary>
+    /// Class for retrieving the appropriate VoiceType enum value from the given string
+    /// </summary>
     public static class VoiceTypeConverter
     {
         private static Dictionary<string, VoiceType> voiceTypeDictionary;
         //todo: add strings/voicetypes to dictionary
 
+        /// <summary>
+        /// Returns the appropriate enum for the inputted string; null if string is not in dictionary.
+        /// </summary>
+        /// <param name="voiceString">The string to convert into VoiceType</param>
         public static VoiceType? getVoiceTypeFromString(string voiceString)
         {
             if(voiceTypeDictionary.ContainsKey(voiceString))
