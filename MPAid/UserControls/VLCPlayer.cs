6a355771d5fbbd7970bb0751dbce42cfb39f7bcf
JSCooke <JaydenCooke@outlook.com>
2017-01-06 12:12:00 +1300

    Implemented repeat functionality in VideoPlayer, and made thread safety improvements that were needed for this function.

--- MPAid/UserControls/VLCPlayer.cs
+++ MPAid/UserControls/VLCPlayer.cs
@@ -21,7 +21,7 @@ namespace MPAid.UserControls
     /// <summary>
     /// Class handling the VLC video player.
     /// </summary>
-    public partial class VlcPlayer : UserControl, INotifyPropertyChanged
+    public partial class VlcPlayer : UserControl
     {
         // All strings are kept here to make them eaiser to change.
         private string selectFolderString = "Select Vlc libraries folder.";
@@ -29,12 +29,24 @@ namespace MPAid.UserControls
         private string noVideoString = "No video recording was found for that sound.";
         private string invalidRecordingString = "Invalid recording!";
 
+        // Used to keep track of the currently playing file.
+        private string filePath;
+
+        // Delegate required for VLC Player.
+        delegate void delegatePlayer(Uri file, string[] pars);
+        delegate void delegateStopper();
+
         // The list of recordings to play.
         private List<Word> wordsList;
         // The index of the current recording.
         private int currentRecordingIndex = 0;
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        private int repeatTimes = 0;
+        private int repeatsRemaining = 0;
+
+        // Events used to send setting changes to the parent class.
+        public event PropertyChangedEventHandler IndexChanged;
+        public event PropertyChangedEventHandler RepeatChanged;
 
         /// <summary>
         /// Wrapper property for the list of recordings.
@@ -51,6 +63,7 @@ namespace MPAid.UserControls
                 wordsList = value;
             }
         }
+
         /// <summary>
         /// Wrapper property for the current index.
         /// </summary>
@@ -63,7 +76,25 @@ namespace MPAid.UserControls
             set
             {
                 currentRecordingIndex = value;
-                NotifyPropertyChanged();
+                NotifyIndexChanged();
+            }
+        }
+
+        /// <summary>
+        /// Wrapper property for the times to repeat a sound.
+        /// </summary>
+        public int RepeatTimes
+        {
+            get
+            {
+                return repeatTimes;
+            }
+
+            set
+            {
+                repeatsRemaining = value;
+                repeatTimes = value;
+                NotifyRepeatChanged();
             }
         }
 
@@ -71,11 +102,23 @@ namespace MPAid.UserControls
         /// Called when the CurrentRecordingIndex property is changed, allowing the parent class to subscribe to the PropertyChanged event and update its own values.
         /// </summary>
         /// <param name="propertyName">Automatically filled in by the CallerMemberName annotation.</param>
-        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        private void NotifyIndexChanged([CallerMemberName] String propertyName = "")
         {
-            if (PropertyChanged != null)
+            if (IndexChanged != null)
             {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+                IndexChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        /// <summary>
+        /// Called when the RepeatTimes property is changed, allowing the parent class to subscribe to the PropertyChanged event and update its own values.
+        /// </summary>
+        /// <param name="propertyName">Automatically filled in by the CallerMemberName annotation.</param>
+        private void NotifyRepeatChanged([CallerMemberName] String propertyName = "")
+        {
+            if (RepeatChanged != null)
+            {
+                RepeatChanged(this, new PropertyChangedEventArgs(propertyName));
             }
         }
 
@@ -115,7 +158,7 @@ namespace MPAid.UserControls
 
             if (!e.VlcLibDirectory.Exists)      // If a folder is missing
             {
-                var folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();       // Raise a browser window and let the user find it.
+                var folderBrowserDialog = new FolderBrowserDialog();       // Raise a browser window and let the user find it.
                 folderBrowserDialog.Description = selectFolderString;
                 folderBrowserDialog.RootFolder = Environment.SpecialFolder.Desktop;
                 folderBrowserDialog.ShowNewFolderButton = true;
@@ -189,9 +232,8 @@ namespace MPAid.UserControls
                         MessageBox.Show(noVideoString);
                         return;
                     }
-                    string filePath = Path.Combine(sf.Address, sf.Name);
-
-                    vlcControl.Play(new Uri(filePath)); // Play the video.
+                    filePath = Path.Combine(sf.Address, sf.Name);
+                    asyncPlay();
                     playButton.ImageIndex = 3;
                 }
                 else
@@ -202,13 +244,25 @@ namespace MPAid.UserControls
         }
 
         /// <summary>
-        /// When the stop button is clicked, put the video into the stopped state.
+        /// The VLCPlayer is not threadsafe, so it is much easier to invoke it with delegates.
         /// </summary>
-        /// <param name="sender">Automatically generated by Visual Studio.</param>
-        /// <param name="e">Automatically generated by Visual Studio.</param>
-        private void stopButton_Click(object sender, EventArgs e)
+        private void asyncPlay()
         {
-            vlcControl.Stop();
+            // Get a new instance of the delegate
+            delegatePlayer VLCDelegate = new delegatePlayer(vlcControl.Play);
+            // Call play asynchronously.
+            VLCDelegate.BeginInvoke(new Uri(filePath), new string[] { }, null, null);
+        }
+
+        /// <summary>
+        /// The VLCPlayer is not threadsafe, so it is much easier to invoke it with delegates.
+        /// </summary>
+        private void asyncStop()
+        {
+            // Get a new instance of the delegate
+            delegateStopper VLCDelegate = new delegateStopper(vlcControl.Stop);
+            // Call stop asynchronously.
+            VLCDelegate.BeginInvoke(null, null);
         }
 
         /// <summary>
@@ -282,14 +336,28 @@ namespace MPAid.UserControls
         }
 
         /// <summary>
-        /// When the video finishes playing, revert the play button to it's original state.
+        /// When the video finishes playing, revert the play button to it's original state, and repeat the video if the user has set it to repeat.
         /// </summary>
         /// <param name="sender">Automatically generated by Visual Studio.</param>
         /// <param name="e">Automatically generated by Visual Studio.</param>
         private void vlcControl_EndReached(object sender, Vlc.DotNet.Core.VlcMediaPlayerEndReachedEventArgs e)
         {
-            // Add repeat/autoplay functionality here
-            playButton.ImageIndex = 1;
+            // The only way to loop playback is to have a delegate call play asynchronously. 
+            if (repeatTimes == 11)
+            {
+                asyncPlay();
+            }
+            else if (repeatsRemaining > 0)
+            {
+                asyncPlay();
+                repeatsRemaining -= 1;
+            }
+            else
+            {
+                repeatsRemaining = RepeatTimes;
+                playButton.ImageIndex = 1;
+                // AutoPlay here
+            }
         }
 
         /// <summary>
@@ -315,7 +383,7 @@ namespace MPAid.UserControls
             }
             else
             {
-                vlcControl.Stop();  // So the old image doesn't stay on screen.
+                asyncStop();  // So the old image doesn't stay on screen.
             }
         }
 
@@ -342,7 +410,7 @@ namespace MPAid.UserControls
             }
             else
             {
-                vlcControl.Stop();  // So the old image doesn't stay on screen.
+                asyncStop();  // So the old image doesn't stay on screen.
             }
         }
     }
