30f5f73bd4ab13ff1de797684f37ed56253bccc6
JSCooke <JaydenCooke@outlook.com>
2017-01-20 10:36:17 +1300

    Added temp to AppDataPath

--- MPAid/Models/AppDataPath.cs
+++ MPAid/Models/AppDataPath.cs
@@ -13,8 +13,28 @@ namespace MPAid.Models
     class AppDataPath
     {
         /// <summary>
+        /// Returns the location of a temporary folder in the temp space on the user's machine.
+        /// </summary>
+        private readonly static string temp = Path.Combine(System.Environment.GetEnvironmentVariable("TEMP"), "MPAiTemp");
+
+        /// <summary>
         /// Combination of the standard MPAi directory, and a folder which will always be called App_Data.
         /// </summary>
         public readonly static string path = Path.Combine(System.Environment.GetEnvironmentVariable("localappdata"),"MPAi");
+
+        /// <summary>
+        /// Instantiates the temp directory if it doesn't already exist.
+        /// </summary>
+        public static string Temp
+        {
+            get
+            {
+                if (!Directory.Exists(temp))
+                {
+                    Directory.CreateDirectory(temp);
+                }
+                return temp;
+            }
+        }
     }
 }
